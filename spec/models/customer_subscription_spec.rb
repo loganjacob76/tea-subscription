ec1819ad55d298e82989d5554f522cd0a6ec995d
loganjacob76 <nagolisback@gmail.com>
2021-07-28 01:35:19 -0600

    Update validations

--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe CustomerSubscription do
   describe 'validations' do
     it { should validate_presence_of :customer_id }
     it { should validate_presence_of :subscription_id }
-    it { should validate_presence_of :active }
  end
 
   describe 'relationships' do
