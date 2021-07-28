3729afa61954e91eaf8d53a4b1855ca36837c2da
loganjacob76 <nagolisback@gmail.com>
2021-07-28 02:17:45 -0600

    Update factory frequency

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :subscription do
     title { Faker::Subscription.plan }
     price { rand(5.00..25.00).round(2) }
-    frequency { rand(1..5) }
+    frequency { rand(0..4) }
   end
 end
