3d40d81bcfd87b8a959232361f6d9e6deac27ff2
loganjacob76 <nagolisback@gmail.com>
2021-07-28 02:11:45 -0600

    Update factory price

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription do
     title { Faker::Subscription.plan }
-    price { rand(5.00..25.00) }
+    price { rand(5.00..25.00).round(2) }
     frequency { rand(1..5) }
   end
 end
